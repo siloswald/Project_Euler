7e8eea750c32d27da75ae2ac6b79338ed38aebd8
Andrew Hainer <ahainer123@gmail.com>
2018-04-03 21:51:43 -0400

    Update 1euler.js

--- 1euler.js
+++ 1euler.js
@@ -14,4 +14,17 @@ return sum; //return variable sum
 console.log(euler_01(1000)) //log function with object literal 1000
 
 //You can add other keywords to loops that allow for different functionality 
+
 //Keywords: do while if for 
+
+//Post-Increment Operand:
+//a++ 
+//add 1 to a, returns the old value.
+//++a 
+//add 1 to a, returns the new value.
+
+//Post-Decrement Operand:
+//a--
+//subtract 1 to a, returns the old value
+//--a
+//add 1 to a, returns the new value
